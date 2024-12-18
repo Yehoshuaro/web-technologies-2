bf76324af3b4636ad30b26d1814b0e51584cc436
Yerkhan <231132@astanait.edu.kz>
2024-12-18 23:27:04 +0500

    easy server + qr code logic

--- index.js
+++ index.js
@@ -1,6 +1,46 @@
-var http = require('http');
+const express = require("express");
+const QRCode = require("qrcode");
+const PORT = 3000;
+const app = express();
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
-}).listen(7755);
+app.get("/", (req, res) => {
+    const url = "https://www.npmjs.com/";
+
+    QRCode.toDataURL(url, (err, qrCode) => {
+        if (err) {
+            res.send("Error with QR Code" + err);
+            return;
+        }
+
+        res.send(`
+            <html>
+            <head>
+                <title>QR Code</title>
+                <style>
+                    body {
+                        text-align: center;
+                        font-family: Arial, sans-serif;
+                        margin-top: 50px;
+                    }
+                </style>
+            </head>
+            <body>
+                <h1>QR to the npmjs.com</h1>
+                <img src="${qrCode}" alt="QR Code" />
+            </body>
+            </html>
+        `);
+    });
+});
+
+app.listen(PORT, () => {
+    console.log("http://localhost:" + PORT);
+});
+
+
+
+
+
+
+
+
