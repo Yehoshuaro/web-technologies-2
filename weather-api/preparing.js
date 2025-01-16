f01b3f6af754eeb4029f640de55b86a429a9b19c
Yerkhan <231132@astanait.edu.kz>
2025-01-16 22:34:45 +0500

    assignment 2 changes: changing preparing to defence codes

--- weather-api/preparing.js
+++ weather-api/preparing.js
@@ -3,48 +3,28 @@ const axios = require("axios");
 const app = express();
 
 require("dotenv").config();
-const PORT = 3000;
 const API_KEY = process.env.OPENWEATHER_API_KEY;
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/preparing.html");
-});
+app.get("/", (req, res) =>
+    res.sendFile(__dirname + "/preparing.html"));
 
 app.get("/weather", async (req, res) => {
     const city = req.query.city;
-
-    if (!city) {
-        return res.status(400).send("City is required");
-    }
+    if (!city) return res.status(400).send("City is required");
 
     try {
-        const response = await axios.get(
+        const { data } = await axios.get(
             `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
         );
-        const data = response.data;
-
         res.json({
-            city: data.name,
-            country: data.sys.country,
             temperature: data.main.temp,
             feels_like: data.main.feels_like,
-            humidity: data.main.humidity,
-            pressure: data.main.pressure,
-            wind_speed: data.wind.speed,
-            coordinates: {
-                lat: data.coord.lat,
-                lon: data.coord.lon,
-            },
             description: data.weather[0].description,
-            icon: `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`,
         });
     }
-    catch (error) {
+    catch {
         res.status(500).send("Error fetching weather data");
     }
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
-});
-
+app.listen(3000, () => console.log("Server running on http://localhost:3000"));
